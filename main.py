0a31b27ff6c7691865f29086b202a8c232850a09
adiz50 <adribox12@gmail.com>
2023-05-25 17:27:00 +0200

    fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 P = [0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344, 0xa4093822, 0x299f31d0, 0x082efa98, 0xec4e6c89, 0x452821e6,
      0x38d01377, 0xbe5466cf, 0x34e90c6c, 0xc0ac29b7, 0xc97c50dd, 0x3f84d5b5, 0xb5470917, 0x9216d5d9, 0x8979fb1b]
 
-key = "0xaabb09182736ccdd"
+key = "0xfbda09182736c1d4"
 
 
 def uploadSbox(sboxArray, src):
@@ -90,12 +90,12 @@ def bitsToStr(B):
 def encryptionRound(value, i, subkeys):
     i = i % len(subkeys)
     firstpart, secondpart = value[:len(value) // 2], value[len(value) // 2:]
-    a = int(firstpart, 2)
-    b = int(secondpart, 2)
+    L = int(firstpart, 2)
+    R = int(secondpart, 2)
     # print(subkeys)
-    aNew = int(subkeys[i]) ^ a
-    b = int(subkeys[i]) ^ int(functionF(format(aNew, '032b')), 2)
-    return format(b, '032b') + format(a, '032b')
+    aNew = int(subkeys[i]) ^ L
+    R = R ^ int(functionF(format(aNew, '032b')), 2)
+    return format(R, '032b') + format(L, '032b')
 
 def postProcessing(roundsOutput, subkeys):
     x_left = int(roundsOutput[:32], 2) ^ int(subkeys[1])
